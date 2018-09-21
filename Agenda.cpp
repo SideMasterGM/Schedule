71ec5e5656b059b7707807e13255bf2227fcf55b
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-09-20 20:19:10 -0600

    Creating some functions

--- Agenda.cpp
+++ Agenda.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <windows.h>
 
 #define CANTIDAD 500
 
@@ -25,6 +25,8 @@ struct Agenda {
 		string Email;
 	} Ema[2];
 	
+	Agenda(); // Declaración del constructor
+	
 } Contacto[CANTIDAD];
 
 int Menu();
@@ -33,22 +35,38 @@ void Buscar();
 void Actualizar();
 void Eliminar();
 
+int Salir();
+
 void Detenerse();
+void LimpiarPantalla();
+void Dormir(int);
+
+// Definición del constructor, fuera de la estructura
+Agenda::Agenda() {
+	Nombre = " ";
+	Tel[0].Telefono = "0";
+	Cel[0].Celular = "0";
+	Ema[0].Email = " ";
+}
 
 int main(int argc, char *argv[]) {
-	int N;
+	int x;
 	
 	Menu:
 		do {
-			N = Menu();
-		} while(N < 1 || N > 2);
+			x = Menu();
+		} while(x < 1 || x > 3);
 	
-	switch (N){
+	switch (x){
 		case 1: 
 			Insertar();
 			break;
 		case 2:
 			Buscar();
+			break;
+		case 3: 
+			if (Salir() == 1)
+				goto Finish;
 			break;
 		default:
 			printf("Up's, ha ocurrido algo inesperado, presione una tecla para continuar!.");
@@ -58,19 +76,29 @@ int main(int argc, char *argv[]) {
 	
 	goto Menu;
 	
-	return 0;
+	Finish: 
+		return 0;
 }
 
 int Menu(){
 	int x;
 	
-	system("CLS");
+	LimpiarPantalla();
 	
 	cout << Regla << endl;
 	cout << "|\tBienvenido a tu Agenda Electrónica (Contactos)\t    |" << endl;
 	cout << Regla << endl;
 	
-	cout << "|     (1) Nuevo contacto      |      (2) Buscar contacto    |" << endl;
+	if (Contacto[0].Nombre == " "){
+		Dormir(1);
+		cout << "|\t\t\t\t\t\t\t    |" << endl;
+		cout << "|\t\t       No hay contactos\t\t\t    |" << endl;
+		cout << "|\t\t\t\t\t\t\t    |" << endl;
+		Dormir(1);
+	}
+	
+	cout << Regla << endl;
+	cout << "| (1) Nuevo contacto  |  (2) Buscar contacto  | (3) Salir   |" << endl;
 	cout << Regla << endl;
 	
 	cout << "Esperando respuesta: ";
@@ -101,4 +129,21 @@ void Eliminar(){
 void Detenerse(){
 	cout << "¡Presione una tecla para continuar!";
 	system("PAUSE>NUL");
+}
+void LimpiarPantalla(){
+	system("CLS");
+}
+void Dormir(int x){
+	Sleep(x * 1000);
+}
+int Salir(){
+	int x;
+	
+	do {
+		cout << "\n\t¿Está seguro de desear salir?" << endl;
+		cout << "\t(1) Sí, (2) No: ";
+		scanf("%d", &x); fflush(stdin);
+	} while(x < 1 || x > 2);
+	
+	return x;
 }
